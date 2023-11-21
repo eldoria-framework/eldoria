0dd1293f6800a118ac32cae17a6140e2b5ba13d2
Leandro <Leandro.Viveiros-ext@ab-Inbev.com>
2023-11-21 14:12:11 -0300

    addded eldoria amulet auto config

---
+++ eldoria-amulet/src/main/java/dev/eldoria/amulet/EldoriaAutoConfiguration.java
@@ -0,0 +1,9 @@
+package dev.eldoria.amulet;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("dev.eldoria")
+public class EldoriaAutoConfiguration {
+}
